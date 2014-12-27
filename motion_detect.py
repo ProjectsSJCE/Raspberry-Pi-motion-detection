0ef720cad688fc5f739421542a9a61a839223906
Sharath TS <sharath@sharath-HP-ProBook-4510s.(none)>
2014-12-27 18:27:01 +0530

    almost done

--- motion_detect.py
+++ motion_detect.py
@@ -1,55 +1,68 @@
 import cv2
-import numpy as np
-threshold = 85000
-def diffImg(t0, t1, t2):
-    d1 = cv2.absdiff(t2, t1)
-    d2 = cv2.absdiff(t1, t0)
-    return cv2.bitwise_and(d1, d2)
-	 
-cam = cv2.VideoCapture(0)
-	 
-winName = "Movement Indicator"
-cv2.namedWindow(winName, cv2.CV_WINDOW_AUTOSIZE)
-	 
-	# Read three images first:
-t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-new_image = diffImg(t_minus, t, t_plus)
-print len(new_image)
-print len(new_image[0])
-count = 0	 
-while True:
-    new_image = diffImg(t_minus, t, t_plus)
 
-    diff = sum(sum(abs(new_image)))
-    minimum = np.argmin(new_image[:, :])
-    maximum = np.argmax(new_image[:, :])
-    
-    print "hello"
-    print diff
-    print minimum
-    print maximum
-    
-#    changed = 0
-#    for x in np.nditer(diff):
-#        for y in np.nditer(diff[0]):
-#            if diff[x][y] > threshold:
-#                changed += 1
-#    print changed
-    if diff > threshold:
-        print "motion", count
-        count += 1
-        
-    
-    cv2.imshow( winName, new_image )
-    
-	  # Read next image
-    t_minus = t
-    t = t_plus
-    t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+cam = cv2.VideoCapture(0) #Set camera parameters
+#threshold_sum = 85000
+threshold_max = 180
+
+#Gets the differential images given the previous, current and the next frame
+def get_differential_image(frame_before, frame_now, frame_after):
+
+    differential_1 = cv2.absdiff(frame_after, frame_now)
+    differential_2 = cv2.absdiff(frame_now, frame_before)
+    return cv2.bitwise_and(differential_1, differential_2)
 	 
-    key = cv2.waitKey(10)
-    if key == 27:
-        cv2.destroyWindow(winName)
-        break
+#Gets the initial three consecutive frames
+def get_three_frames():
+
+    global cam
+    frame_before = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+    frame_now = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+    frame_after = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+    return [frame_before, frame_now, frame_after]
+    
+#Main function
+def main():
+
+    
+    global cam, threshold_max, threshold_sum 
+    cv2.namedWindow("Differential Image", cv2.CV_WINDOW_AUTOSIZE)
+	     
+    #Capture initial three frames and convert them to RGB
+    [frame_before,frame_now, frame_after] = get_three_frames()
+    
+
+#    print len(req_image)
+#    print len(req_image[0])
+    count = 0	 
+
+    while True:
+
+#       Getting the differential image
+        req_image = get_differential_image(frame_before, frame_now, frame_after)
+
+#        Calculate sum, minimum and maximum of the pixel values of the differential image
+        sum_image = req_image.sum()
+        minimum = req_image.min()
+        maximum = req_image.max()
+        
+#        print "sum ", sum_image
+#        print "min ", minimum
+#        print "max", maximum
+        
+#        if maximum value of the differential image is greater than the threshold of maximum value, declare that there is motion
+        if maximum > threshold_max: #This threshold value has to be adjusted based on the sensitivity required
+            print "motion", count
+            print "max ", maximum
+            count += 1
+            
+        
+        cv2.imshow("Differential Image", req_image )
+        
+#       Capture next frame (i.e., shift right)
+        frame_before = frame_now
+        frame_now = frame_after
+        frame_after = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+	     
+        key = cv2.waitKey(10)
+
+main()
